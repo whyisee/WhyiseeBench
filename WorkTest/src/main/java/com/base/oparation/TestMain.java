19c840f22b221e10cbc74c8b269471c8a4168b28
zoukh <907022598@qq.com>
2020-05-15 18:20:49 +0800

    操作符练习  这周末突击英语

---
+++ WorkTest/src/main/java/com/base/oparation/TestMain.java
@@ -0,0 +1,24 @@
+package com.base.oparation;
+
+/**
+ * use for :
+ *
+ * @author zoukh
+ * Created in:  2020/5/15 17:16
+ * @version 1.0
+ * @Modified By:
+ * @used in: WhyiseeBench
+ */
+public class TestMain {
+    public static void main(String args[]){
+        int i = 0b11111;
+
+        String str;
+        //i = i & 1;
+        System.out.println("Test--------17:28--->:"+0b1000);
+        System.out.println("Test--------17:16--->:"+(1) / 8);
+        byte b = 0b11 ;
+        System.out.println("Test--------17:21--->:"+b);
+        System.out.println("Test--------18:00--->:"+"看".compareTo("3"));
+    }
+}
