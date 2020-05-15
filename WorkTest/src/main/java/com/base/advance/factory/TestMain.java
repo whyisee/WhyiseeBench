3e7339a171d16865ef03166b9e005a5a1cba4fb4
zoukh <907022598@qq.com>
2020-05-15 08:10:04 +0800

    advence

---
+++ WorkTest/src/main/java/com/base/advance/factory/TestMain.java
@@ -0,0 +1,13 @@
+package com.base.advance.factory;
+
+import java.util.EnumSet;
+
+public class TestMain {
+    public static void main(String[] args){
+        boolean bool1 = true;
+        Boolean bool2 =  Boolean.valueOf("true");
+        System.out.println(bool2== bool1);
+        //EnumSet;
+        //bool2.
+    }
+}
